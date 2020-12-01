4e9858bcef5a991d48b04089c5eb8d0c2bb639fa
Giovanni Compagnoni <gcompagnoni@nextbit.it>
2020-12-01 19:43:50 +0100

    1.1

---
+++ problem_1_1.py
@@ -0,0 +1,25 @@
+with open('input_1_1.txt', 'r') as infile:
+    s = [int(x) for x in infile.readlines()]
+
+s.sort()
+my_sum = 2020
+
+
+def recursive_binary_search(r, x):
+    if len(r) == 0:
+        return None
+    y_index = len(r) // 2
+    y = r[y_index]
+    if x + y == my_sum:
+        return y
+    elif x + y < my_sum:
+        return recursive_binary_search(r[y_index + 1:], x)
+    elif x + y > my_sum:
+        return recursive_binary_search(r[:y_index], x)
+
+
+for k, x in enumerate(s):
+    y = recursive_binary_search(s[k + 1:], x)
+    if y is not None:
+        print(x, y, x * y)
+        break
