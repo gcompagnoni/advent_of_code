4d6b78cf20e7c205aee412931c14b10eaa602620
Giovanni Compagnoni <gcompagnoni@nextbit.it>
2020-12-05 16:32:27 +0100

    5.2

--- day_5_2.py
+++ day_5_2.py
@@ -10,10 +10,14 @@ def str2place(input_string: str) -> Tuple[int, int]:
     return int(row_binary, 2), int(col_binary, 2)
 
 
-max_seat_id = 0
+seat_ids = set()
 for seat in seat_list:
     row_num, col_num = str2place(seat)
     seat_id = 8 * row_num + col_num  # this is the same as just reading the whole string as binary
-    max_seat_id = max(max_seat_id, seat_id)
+    seat_ids.add(seat_id)
 
-print(max_seat_id)
+no_front = set(range(min(seat_ids)))
+no_back = set(range(max(seat_ids) + 1, 2 ** 10))
+my_seat = set(range(2 ** 10)).difference(seat_ids).difference(no_front).difference(no_back)
+
+print(my_seat)
