4794a31e971341003f9b8c70508e8c3f69c283b0
Giovanni Compagnoni <gcompagnoni@nextbit.it>
2020-12-06 17:23:43 +0100

    6.2

---
+++ day_6_2.py
@@ -0,0 +1,11 @@
+with open('inputs/input_6.txt', 'r') as infile:
+    groups = infile.read().split('\n\n')
+
+individual_questions = [[set(q) for q in g.split('\n')] for g in groups]
+
+num_questions = 0
+
+for group_questions in individual_questions:
+    num_questions += len(set.intersection(*group_questions))
+
+print(num_questions)
