75841f4784eeab077ec4fce8306d976c2f8f83f1
Giovanni Compagnoni <gcompagnoni@nextbit.it>
2020-12-06 17:10:27 +0100

    6.1

---
+++ day_6_1.py
@@ -0,0 +1,11 @@
+with open('inputs/input_6.txt', 'r') as infile:
+    groups = infile.read().split('\n\n')
+
+unique_questions = [set(g.replace('\n', '')) for g in groups]
+
+num_questions = 0
+
+for group_questions in unique_questions:
+    num_questions += len(group_questions)
+
+print(num_questions)
