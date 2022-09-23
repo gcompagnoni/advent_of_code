427be3017fb2ac482f53c6b86c7939542d53d927
Giovanni Compagnoni <gcompagnoni@nextb.it>
2022-09-23 18:46:30 +0200

    day 19 pt 2

--- day_19_2.py
+++ day_19_2.py
@@ -1,8 +1,6 @@
 from typing import Union
 
 
-# this looks like a way to define some non-deterministic finite automata
-
 # treat the message as being "consumed" by advancing through a rule
 def check_message(msg: str, rule: list[list[int]]) -> bool:
     if rule == [[]] and msg == '':
@@ -40,6 +38,18 @@ def check_message(msg: str, rule: list[list[int]]) -> bool:
     return False
 
 
+# only modification to deal with loops is that we need to make sure that
+# the non-looping options (there must always be at least one) for a rule come before the looping ones
+def reorder_rule(rule_id: int) -> None:
+    if isinstance(rule_dict[rule_id], list):
+        new_rule = rule_dict[rule_id].copy()
+        for subrule in rule_dict[rule_id]:
+            if rule_id in subrule:
+                new_rule.remove(subrule)
+                new_rule.append(subrule)
+        rule_dict[rule_id] = new_rule
+
+
 def rule_as_list(rule_str: str) -> Union[str, list[list[int]]]:
     # assume rules are composed either by other rules or by a single quoted string
     if rule_str.find('"') >= 0:
@@ -52,6 +62,13 @@ with open('inputs/input_19.txt', 'r') as infile:
     rule_list, messages = [item.split('\n') for item in infile.read().split('\n\n')]
 
 rule_dict = dict(map(lambda x: (int(x[0]), rule_as_list(x[1])), [r.split(':') for r in rule_list]))
+
+# apply modification to specified rules
+rule_dict[8] = [[42], [42, 8]]
+rule_dict[11] = [[42, 31], [42, 11, 31]]
+
+for idx in rule_dict.keys():
+    reorder_rule(idx)
 
 responses = [check_message(msg, [[0]]) for msg in messages]
 
